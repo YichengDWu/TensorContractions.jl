1b9e5758df6c64260894efd5a319708a993c9256
Documenter.jl <documenter@juliadocs.github.io>
2023-06-17 19:23:36 +0000

    build based on d9cacd5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TensorContractions","category":"page"},{"location":"#TensorContractions","page":"Home","title":"TensorContractions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TensorContractions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TensorContractions]","category":"page"}]
+}
